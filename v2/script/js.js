63ababc10ba924b6ada847fcbbc180ed853fadd1
fanweiGit <weiwei.fanchn@foxmail.com>
2022-11-07 10:21:44 +0800

    k :# Please enter the commit message for your changes. Lines starting

--- v2/script/js.js
+++ v2/script/js.js
@@ -3,19 +3,19 @@ let date = new Date()
 let td = new Date()
 //开始时间，当前时间前30min
 td.setMinutes(date.getMinutes() - 30)
-$id('kssj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay()-1)} ${zero(td.getHours())}:${zero(td.getMinutes())}`
+$id('kssj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 //结束时间，当前时间后2个小时
 td.setHours(date.getHours() + 1)
-$id('jssj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay()-1)} ${zero(td.getHours())}:${zero(td.getMinutes())}`
+$id('jssj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 
 //发起申请的时间,2小时38分钟之前
 td.setHours(date.getHours() - 2)
 td.setMinutes(td.getMinutes() - 38)
-$id('fqsj').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay()-1)} ${zero(td.getHours())}:${zero(td.getMinutes())}`
+$id('fqsj').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 //审批通过时间，1小时19分钟之前
 td.setHours(date.getHours() - 1)
 td.setMinutes(td.getMinutes() - 19)
-$id('spsj').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay()-1)} ${zero(td.getHours())}:${zero(td.getMinutes())}`
+$id('spsj').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 
 //如何销假？
 $id('rhxj').onclick = ()=>{
